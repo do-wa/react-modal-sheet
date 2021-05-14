e3ee3aa5aeeb70fe7ed5738ec552e542f69a88d8
Dominik Walnsch <d.walnsch@outlook.de>
2021-05-14 20:27:09 +0200

    conver to lazy framer motion

--- src/SheetHeader.tsx
+++ src/SheetHeader.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { motion, useTransform } from 'framer-motion';
+import { m as motion, useTransform } from 'framer-motion';
 
 import { SheetDraggableProps } from './types';
 import { useSheetContext } from './context';
